60c3d1ac5a2cc2b641ec4559f8dac8fbe3adb118
John Tribolet <john@tribolet.info>
2016-08-24 14:51:27 -0400

    Fix silent error when steamId is set as number but larger than MAX_SAFE_INTEGER, throws instead. Add test to ClientSpec.

--- specs/ClientSpec.js
+++ specs/ClientSpec.js
@@ -1,7 +1,7 @@
 var api = require('../steam/api');
 describe('Client Specs / ', function(){
   jasmine.getEnv().defaultTimeoutInterval = 30000;
-  
+
   it('Should squawk if no steam api', function(){
     oldEnv = process.env;
     process.env = {};
@@ -17,5 +17,12 @@ describe('Client Specs / ', function(){
   it('Should squawk if no arguments are found in setupService', function(){
     var client = new api.Client('this-was-set');
     expect(function(){ client.setupService(); }).toThrow(new Error("arguments must be defined"));
+  });
+
+  it('Should squawk when SteamID is trying to set as a number > MAX_SAFE', function(){
+    var client = new api.Client('this-was-set');
+
+    expect(function(){ client.setSteamId(99007199254740991); })
+        .toThrow(new Error('SteamId should be a string when larger than MAX_SAFE_INTEGER 9007199254740991'));
   });
 });
